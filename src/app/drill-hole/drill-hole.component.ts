013dc894fc7f0cee55deddfab05567b7f813650e
Anirban Talukdar <talukdar.anirban@gmail.com>
2025-02-12 10:20:05 +0530

    Merged drill and slot

--- src/app/drill-hole/drill-hole.component.ts
+++ src/app/drill-hole/drill-hole.component.ts
@@ -24,7 +24,7 @@ export class DrillHoleComponent {
   public generateCodes(){
     this.gcodeTextControl.setValue('');
     let stockWidth = -30;
-    let stockCount = 10;
+    let stockCount = 8;
     let cutterWidth = this.cutterWidthControl.value ? this.cutterWidthControl.value : 6;
     let stockDir = stockWidth/Math.abs(stockWidth); 
     let xPos = 0;
@@ -33,11 +33,12 @@ export class DrillHoleComponent {
     for(let i=0; i<stockCount; i++){
       for(let j=0; j<xPositions.length; j++){
         xPos = xPositions[j];
-        this.drillAt(xPos, yPos, 10, -2, 1);
+        this.drillAt(xPos, yPos, 5, -5, 7);
         this.appendText("\n\n");
       }
       yPos += stockWidth;
     }
+    this.moveTo(0, 0);
   }
 
   public moveTo(xPos: number, yPos: number){
@@ -50,10 +51,10 @@ export class DrillHoleComponent {
     this.moveTo(centerX, centerY);
     let cutterWidth = this.cutterWidthControl.value ? this.cutterWidthControl.value : 0;
     let effRadius = radius - cutterWidth/2;
-    this.appendText("\nG00 X" + (centerX + radius));
-    for(let i=0; i<zCount; i++){
-      this.appendText("\nG01 Z" + (this.zPos + zStep));
-      this.appendText("\nG02 X" + (centerX + effRadius) + " Y" + centerY + " I" + effRadius * (-1) + "J0");
+    for(let i=1; i<=zCount; i++){
+      this.appendText("\nG01 Z" + (this.zPos + (zStep*i)));
+      this.appendText("\nG00 X" + (centerX - effRadius));
+      this.appendText("\nG02 X" + (centerX - effRadius) + " Y" + centerY + " I" + effRadius + "J0");
     }
     this.appendText("\nG00 Z" + this.zPos);
   }
--- src/app/multi-slot/multi-slot.component.ts
+++ src/app/multi-slot/multi-slot.component.ts
@@ -3,6 +3,32 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
+
+export class DowelSlot {
+  xPosition: number;
+  dowels: Array<number>;
+  stockWidth: number;
+  slotWidth: number;
+  slotLength: number;
+
+  public constructor(x: number, stockWidth: number, slotWidth: number, slotLength: number){
+    this.xPosition = x;
+    this.dowels = new Array<number>();
+    this.slotWidth = slotWidth;
+    this.stockWidth = stockWidth;
+    this.slotLength = slotLength;
+  }
+
+  public addDowel(x: number){
+    this.dowels.push(x);
+  }
+
+  public addDowels(xPostions: number[]){
+    xPostions.forEach(x => {
+      this.dowels.push(x);
+    });
+  }
+}
 @Component({
   selector: 'app-multi-slot',
   standalone: true,
@@ -28,42 +54,48 @@ export class MultiSlotComponent {
   zPos = 0;
 
   public generateCodes(){
-    let xPos : number = this.xPositionControl.value ? this.xPositionControl.value : 0;
-    let yPos : number = this.yPositionControl.value ? this.yPositionControl.value : 0;
     // TODO validation checking is a must  here
     let cutterWidth : number = this.cutterWidthControl.value ? this.cutterWidthControl.value : 6;
     let xDirLength : number = this.xDirLengthControl.value ? this.xDirLengthControl.value : 0;
     let yDirLength : number = this.yDirLengthControl.value ? this.yDirLengthControl.value : 0;
     let zDepth : number = this.zDepthControl.value ? this.zDepthControl.value : 0;
     let zStep : number = this.zStepControl.value ? this.zStepControl.value : 0;
-    let stockCount = 8;
-    let stockWidth = -30
-    
-    
-    ;
+    let stockCount = 4;
+    //let stockWidth = -30;
     this.gcodeTextControl.setValue('');
 
-    xPos = 0;
-    let stockDir = stockWidth/Math.abs(stockWidth);
+    let dowelSlots = new Array<DowelSlot>();
+
+    let dowelSlot = new DowelSlot(0, -30, -10, 25.4*3.5);
+    dowelSlots.push(dowelSlot);
+    dowelSlot.addDowels([25, 75]);
+
+    dowelSlot = new DowelSlot(1005,-30.5, -10, 25.4*4.5);
+    dowelSlot.addDowels([40, 90]);
+    dowelSlots.push(dowelSlot);
+
     let xDir = 1;
-    let slotWidth = stockWidth/3;
 
-    yPos = yPos + (stockWidth - slotWidth)/2 + cutterWidth * stockDir;
+    let yPos = 0;
 
-    let xPositions = [10];
-    for(let i=0; i<stockCount; i++){
-      for(let j=0; j < xPositions.length; j++){
-        xPos = xPositions[j];
-        this.cutSlot(xPos + xDir * cutterWidth/2, yPos, 83, slotWidth - cutterWidth * stockDir, -2, 1);
-        yPos += stockWidth;
+    for(let i=0; i < dowelSlots.length; i++){
+      let dowelSlot = dowelSlots[i];
+      let stockDir = dowelSlot.stockWidth/Math.abs(dowelSlot.stockWidth);
+      yPos = (dowelSlot.stockWidth - dowelSlot.slotWidth)/2 + cutterWidth * stockDir;
+      for(let j=0; j<stockCount; j++){
+        this.cutSlot(dowelSlot.xPosition + xDir * cutterWidth/2, yPos, dowelSlot.slotLength - xDir*cutterWidth, dowelSlot.slotWidth - cutterWidth * stockDir, -4.3, 3);
+        let dowelYPos = yPos + dowelSlot.slotWidth/2 - stockDir*cutterWidth/2;
+        for(let k=0; k<dowelSlot.dowels.length; k++){
+          let dowelXPos = dowelSlot.xPosition + dowelSlot.dowels[k];
+          this.drillAt(dowelXPos, dowelYPos, 5, -5, 7);
+        }
+        yPos += dowelSlot.stockWidth;
       }
     }
-    this.appendText("\nG00 X0Y0");
-    this.appendText("\nG00 Z0");
+    this.moveTo(0, 0);
   }
 
   private cutSlot(xPos: number, yPos: number, xLength: number, yLength: number, zStep: number, zCount: number): void{
-    //this.appendText('\n;generating gcode for xPos = ' + xPos + ", yPos=" + yPos + ", xLength=" + xLength + ", yLength" + yLength + ", zStep=" + zStep + ", zCount=" + zCount);
     this.moveTo(xPos, yPos);
 
     let z = this.zPos;
@@ -77,6 +109,18 @@ export class MultiSlotComponent {
     }
     this.appendText("\nG00 Z" + this.zPos);
     this.appendText("\n\n")
+  }
+
+  private drillAt(centerX: number, centerY: number, radius: number, zStep: number, zCount: number){
+    this.moveTo(centerX, centerY);
+    let cutterWidth = this.cutterWidthControl.value ? this.cutterWidthControl.value : 0;
+    let effRadius = radius - cutterWidth/2;
+    for(let i=1; i<=zCount; i++){
+      this.appendText("\nG01 Z" + (this.zPos + (zStep*i)));
+      this.appendText("\nG00 X" + (centerX - effRadius));
+      this.appendText("\nG02 X" + (centerX - effRadius) + " Y" + centerY + " I" + effRadius + "J0");
+    }
+    this.appendText("\nG00 Z" + this.zPos);
   }
 
   public moveTo(xPos: number, yPos: number){
